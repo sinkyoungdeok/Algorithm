849b5d901e1c348ec2a4e309ecf80122e7e06486
sinkyoungdeok <tlsrudejr123@nate.com>
2019-03-18 00:24:22 +0900

    별찍기-12

---
+++ boj_code/2500/2522.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n - i; j > 0; j--)
+			cout << " ";
+		for (int j = i; j > 0; j--)
+			cout << "*";
+		cout << endl;
+	}
+	for (int i = n - 1; i > 0; i--)
+	{
+		for (int j = n - i; j > 0; j--)
+			cout << " ";
+		for (int j = i; j > 0; j--)
+			cout << "*";
+		cout << endl;
+	}
+	return 0;
+}

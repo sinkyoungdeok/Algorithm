944b8088a05ade0e1ef83b9e7d9a097a4f7655b9
sinkyoungdeok <tlsrudejr123@nate.com>
2019-03-18 00:39:33 +0900

    구구단

---
+++ boj_code/2700/2739.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 0;
+	cin >> a;
+	for (int i = 1; i <= 9; i++)
+	{
+		cout << a << " * " << i << " = " << a * i << endl;
+	}
+	//system("pause");
+	return 0;
+}

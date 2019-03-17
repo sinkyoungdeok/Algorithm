3d61fe215ca56bd750c010ac284e2030ad3a0826
sinkyoungdeok <tlsrudejr123@nate.com>
2019-03-18 00:50:39 +0900

    달팽이는 올라가고 싶다

---
+++ boj_code/2800/2869.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <functional>
+#include <cstdlib>
+#include <map>
+using namespace std;
+int main()
+{
+	long long int A, B, V;
+	cin >> A >> B >> V;
+	cout << (V - A - 1) / (A - B) + 2 << endl;
+
+
+
+
+	return 0;
+}

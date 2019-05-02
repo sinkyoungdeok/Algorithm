fff3fcaf9e60eeba2e5dae7b3ef11a6091d55a0a
sinkyoungdeok <tlsrudejr123@nate.com>
2019-05-02 23:34:32 +0900

    웰컴

---
+++ boj_code/5300/5337.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	cout << ".  .   ." << endl;
+	cout << "|  | _ | _. _ ._ _  _" << endl;
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+	return 0;
+}

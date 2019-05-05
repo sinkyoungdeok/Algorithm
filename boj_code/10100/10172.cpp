aae829d91c4676cc25f3f16ee02608139c0dd950
sinkyoungdeok <tlsrudejr123@nate.com>
2019-05-05 21:14:12 +0900

    개

---
+++ boj_code/10100/10172.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\\ " << endl;
+	cout << "|\"\^\"\`    | " << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+	
+
+
+	return 0;
+}

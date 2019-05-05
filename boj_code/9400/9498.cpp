411d39defff9fe8617b75e60524ba01ac53b4b9c
sinkyoungdeok <tlsrudejr123@nate.com>
2019-05-05 21:05:05 +0900

    시험 성적

---
+++ boj_code/9400/9498.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int score = 0;
+	cin >> score;
+	if (score <= 100 && score >= 90)
+		cout << "A" << endl;
+	else if (score >= 80)
+		cout << "B" << endl;
+	else if (score >= 70)
+		cout << "C" << endl;
+	else if (score >= 60)
+		cout << "D" << endl;
+	else
+		cout << "F" << endl;
+
+
+
+	//system("pause");
+	return 0;
+}

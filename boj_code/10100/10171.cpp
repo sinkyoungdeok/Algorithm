56ab3ef17d6262ba4ce4286a21d9f5f1e729d1e2
sinkyoungdeok <tlsrudejr123@nate.com>
2019-05-05 21:14:01 +0900

    고양이

---
+++ boj_code/10100/10171.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()//97~122
+{
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+	return 0;
+}

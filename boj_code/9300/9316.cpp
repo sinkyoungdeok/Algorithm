ba559edeee3a0209681ae92e2da0ef1b458cc9f7
sinkyoungdeok <tlsrudejr123@nate.com>
2019-05-05 21:04:34 +0900

    Hello Judge

---
+++ boj_code/9300/9316.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cout << "Hello World, Judge " << i + 1 << "!"<< endl;
+	return 0;
+}

157c2992b06d0e08730f7bbf33b58523923abb90
sinkyoungdeok <tlsrudejr123@nate.com>
2019-06-27 19:34:18 +0900

    다면체

---
+++ boj_code/10500/10569.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << 2 + b - a << endl;
+	}
+	return 0;
+}

bf98653640d76be67e2a1e5c78f5d4ab58e2412f
sinkyoungdeok <tlsrudejr123@nate.com>
2019-06-27 19:45:23 +0900

    피보나치 수 5

---
+++ boj_code/10800/10870.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	int *num = new int[45];
+	num[0] = 0;
+	num[1] = 1;
+	num[2] = 1;
+	if (n == 1)
+		cout << num[n] << endl;
+	else if (n == 2)
+		cout << num[n] << endl;
+	else
+	{
+		for (int i = 3; i <= 45; i++)
+		{
+			num[i] = num[i - 1] + num[i - 2];
+		}
+		cout << num[n] << endl;
+
+	}
+
+	return 0;
+}

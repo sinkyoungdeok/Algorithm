1926ff3fb38fbee55231d311c7aadf6d3046a13a
sinkyoungdeok <tlsrudejr123@nate.com>
2019-06-27 19:45:11 +0900

    사칙연산

---
+++ boj_code/10800/10869.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int a, b = 0;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	//system("pause");
+	
+
+
+	return 0;
+}

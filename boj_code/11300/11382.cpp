4948135de8e9fbd0c3df7727eaba351b7dee3791
sinkyoungdeok <tlsrudejr123@nate.com>
2019-07-01 12:21:11 +0900

    꼬마 정민

---
+++ boj_code/11300/11382.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	long long int sum, a, b, c;
+	cin >> a >> b >> c;
+	sum = a + b + c;
+	cout << sum << endl;
+	return 0;
+}

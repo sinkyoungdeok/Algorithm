d4f188bd0c82da90db1971cebd1ff058dcbc983f
sinkyoungdeok <tlsrudejr123@nate.com>
2019-07-01 12:26:00 +0900

    Create 11655.cpp

---
+++ boj_code/11600/11655.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	char *num = new char[101];
+	cin.getline(num, 101);
+	int len = strlen(num);
+	for (int i = 0; i < len; i++)
+	{
+		if ((num[i] > 'm' && num[i] <= 'z') || (num[i] > 'M' && num[i] <= 'Z'))
+			num[i] -= 13;
+
+		else if (('a' <= num[i] && num[i] <= 'm') || ('A' <= num[i] && num[i] <= 'M'))
+		{
+			num[i] += 13;
+		}
+		else
+			continue;
+	}
+	cout << num << endl;
+	return 0;
+}
